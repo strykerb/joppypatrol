fe0e3b564756fa1e50042054173be260b334ac59
Stryker Buffington <sbuffing@ucsc.edu>
2021-04-07 14:11:42 -0700

    basic scene structure added

---
+++ src/scenes/Menu.js
@@ -0,0 +1,12 @@
+class Menu extends Phaser.Scene {
+    constructor() {
+        super("menuScene");
+    }
+
+    create() {
+        this.add.text(20, 20, "Rocket Patrol Menu");
+
+        // Change Scene
+        this.scene.start("playScene");
+    }
+}
