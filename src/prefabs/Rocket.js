a0a5f9c0debef2fec01591fca2332759f2faafb0
Stryker Buffington <sbuffing@ucsc.edu>
2021-04-09 17:04:28 -0700

    Added Enemy Rockets and Collision Detection

--- src/prefabs/Rocket.js
+++ src/prefabs/Rocket.js
@@ -20,5 +20,26 @@ class Rocket extends Phaser.GameObjects.Sprite {
                 this.x += this.moveSpeed;
             }
         }
+
+        // fire button
+        if (Phaser.Input.Keyboard.JustDown(keyF)){
+            this.isFiring = true;
+        }
+
+        // if fired, move rocket upwards
+        if (this.isFiring && this.y >= borderUISize *3 + borderPadding){
+            this.y -= this.moveSpeed;
+        }
+
+        if(this.y <= borderUISize * 3 + borderPadding){
+            this.reset();
+        }
+
+    }
+
+    // reset rocket to ground
+    reset(){
+        this.isFiring = false;
+        this.y = game.config.height - borderUISize - borderPadding;
     }
 }
--- src/scenes/Play.js
+++ src/scenes/Play.js
@@ -4,9 +4,17 @@ class Play extends Phaser.Scene {
     }
 
     preload() {
+        // load images/tile sprites
         this.load.image('rocket', './assets/rocket.png');
         this.load.image('spaceship', './assets/spaceship.png');
         this.load.image('starfield', './assets/starfield.png');
+        // load spritesheet
+        this.load.spritesheet('explosion', './assets/explosion.png', {
+            frameWidth: 64, 
+            frameHeight: 32,
+            startFrame: 0,
+            endFrame: 9
+        });
     }
 
     create() {
@@ -30,16 +38,64 @@ class Play extends Phaser.Scene {
         this.p1Rocket = new Rocket(this, game.config.width/2, game.config.height
              - borderUISize - borderPadding, 'rocket').setOrigin(0.5, 0);
 
-            // define keys
-            keyF = this.input.keyboard.addKey(Phaser.Input.Keyboard.KeyCodes.F);
-            keyR = this.input.keyboard.addKey(Phaser.Input.Keyboard.KeyCodes.R);
-            keyLEFT = this.input.keyboard.addKey(Phaser.Input.Keyboard.KeyCodes.LEFT);
-            keyRIGHT = this.input.keyboard.addKey(Phaser.Input.Keyboard.KeyCodes.RIGHT);
+        // add spaceship (x3)
+        this.ship01 = new Spaceship(this, game.config.width + borderUISize*6, 
+            borderUISize*4, 'spaceship', 0, 30).setOrigin(0, 0);
+        this.ship02 = new Spaceship(this, game.config.width + borderUISize*3, 
+            borderUISize*5 + borderUISize*2, 'spaceship', 0, 20).setOrigin(0, 0);
+        this.ship03 = new Spaceship(this, game.config.width, borderUISize*6 + 
+            borderUISize*4, 'spaceship', 0, 10).setOrigin(0, 0);
+
+        // define keys
+        keyF = this.input.keyboard.addKey(Phaser.Input.Keyboard.KeyCodes.F);
+        keyR = this.input.keyboard.addKey(Phaser.Input.Keyboard.KeyCodes.R);
+        keyLEFT = this.input.keyboard.addKey(Phaser.Input.Keyboard.KeyCodes.LEFT);
+        keyRIGHT = this.input.keyboard.addKey(Phaser.Input.Keyboard.KeyCodes.RIGHT);
+
+        // animation config
+        this.anims.create({
+            key: 'explode',
+            frames: this.anims.generateFrameNumbers('explosion', {
+                start: 0,
+                end: 9, 
+                first: 0
+            }),
+            frameRate: 30
+        });
     }
 
     update() {
         this.starfield.tilePositionX -= starSpeed;
 
         this.p1Rocket.update();
+        this.ship01.update();
+        this.ship02.update();
+        this.ship03.update();
+
+        // check collisions
+        if (this.checkCollision(this.p1Rocket, this.ship01)){
+            this.p1Rocket.reset();
+            this.ship01.reset();
+        }
+        if (this.checkCollision(this.p1Rocket, this.ship02)){
+            this.p1Rocket.reset();
+            this.ship02.reset();
+        }
+        if (this.checkCollision(this.p1Rocket, this.ship03)){
+            this.p1Rocket.reset();
+            this.ship03.reset();
+        }
+    }
+
+    checkCollision(rocket, ship){
+        // AABB collision detection
+        if(rocket.x < ship.x + ship.width && 
+            rocket.x + rocket.width > ship.x && 
+            rocket.y < ship.y + ship.height && 
+            rocket.height + rocket.y > ship.y){
+                return true;
+        } else {
+            return false;
+        }
     }
 }
