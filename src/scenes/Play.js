bf9b814a72de417fe16adfb578b3b25a135cc571
Stryker Buffington <sbuffing@ucsc.edu>
2021-04-13 11:37:08 -0700

    Finished core mechanics

--- src/scenes/Play.js
+++ src/scenes/Play.js
@@ -78,17 +78,34 @@ class Play extends Phaser.Scene {
             },
             fixedWidth: 100
         }
-        console.log(this.p1Score);
         this.scoreLeft = this.add.text(borderUISize + borderPadding, borderUISize + borderPadding*2, this.p1Score, scoreConfig);
+
+        // GAME OVER flag
+        this.gameOver = false;
+
+        // 60-second play clock
+        scoreConfig.fixedWidth = 0;
+        this.clock = this.time.delayedCall(10000, () => {
+            this.add.text(game.config.width/2, game.config.height/2, 'GAME OVER', scoreConfig).setOrigin(0.5);
+            this.add.text(game.config.width/2, game.config.height/2 + 64, 'Press (R) to Restart', scoreConfig).setOrigin(0.5);
+            this.gameOver = true;
+        }, null, this);
     }
 
     update() {
+        // check key input for restart
+        if (this.gameOver && Phaser.Input.Keyboard.JustDown(keyR)) {
+            this.scene.restart();
+        }
+        
         this.starfield.tilePositionX -= starSpeed;
 
-        this.p1Rocket.update();
-        this.ship01.update();
-        this.ship02.update();
-        this.ship03.update();
+        if (!this.gameOver) {               
+            this.p1Rocket.update();         // update rocket sprite
+            this.ship01.update();           // update spaceships (x3)
+            this.ship02.update();
+            this.ship03.update();
+        } 
 
         // check collisions
         if (this.checkCollision(this.p1Rocket, this.ship01)){
@@ -128,5 +145,8 @@ class Play extends Phaser.Scene {
             ship.alpha = 1;
             boom.destroy();
         })
+        // score add and repaint
+        this.p1Score += ship.points;
+        this.scoreLeft.text = this.p1Score; 
     }
 }
