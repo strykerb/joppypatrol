25030b5f16a5669b5599eb61f210ae27d798ce57
Stryker Buffington <sbuffing@ucsc.edu>
2021-04-19 23:56:51 -0700

    added source code comments

--- src/main.js
+++ src/main.js
@@ -1,3 +1,20 @@
+// Rocket Patrol Modification: Joppy Patrol
+//
+// Author: Stryker Buffington
+//
+// Time Taken to complete project: ~6 hours
+//
+// They say mistakes never die... I say that Joppy Script was no mistake. Nathan, I'm sorry for bringing this attrocity into existence.
+//
+// Modification List:
+// 	- Total Aesthetic Overhaul: Redesigned Artwork, UI, and Sound             (60)
+// 	- Added looping background music                                          (5)
+// 	- Implemented a Time Remaining UI element                                 (10)
+// 	- Adds time upon every successful hit (of the farthest ship)              (20)
+// 	- Player can still control rocket after firing                            (5)
+//                                                                            (Total: 100)
+
+
 let config = {
     type: Phaser.CANVAS,
     width: 640,
